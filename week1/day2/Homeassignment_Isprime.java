b3e01f778f20d4b839265f93d4f20cffaad2f05f
Ishwarya0108 <159456068+Ishwarya0108@users.noreply.github.com>
2024-03-14 16:33:18 +0530

    Add files via upload
    Week1 day2 changes

---
+++ week1/day2/Homeassignment_Isprime.java
@@ -0,0 +1,25 @@
+package week1.day2;
+
+public class Homeassignment_Isprime {
+
+	public static void main(String[] args) {
+		
+		int num = 4;
+		int count = 0;
+		
+		for(int i=1; i<=num; i++ )
+		{
+			if (num%i==0)
+				count++;
+		}
+		{
+			if (count==2)
+			{
+				System.out.println("The prime number is:"+num);
+			}
+			else
+			{
+			System.out.println("It is not a prime number:"+num);
+			}
+}}}
+	

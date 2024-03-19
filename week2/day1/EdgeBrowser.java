e995ab4ec0120826dbbe30e961063908551b68da
Ishwarya0108 <159456068+Ishwarya0108@users.noreply.github.com>
2024-03-19 22:56:13 +0530

    Add files via upload
    Week2 day 1 files

---
+++ week2/day1/EdgeBrowser.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class EdgeBrowser {
+
+	public static void main(String[] args) {
+		
+		BrowserMethodsandobj Obj = new BrowserMethodsandobj();
+		
+		System.out.println(Obj.browsername("Microsoft Edge"));
+		Obj.loadurl();
+		
+		
+	}
+}

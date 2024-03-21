2e354c442b344125f052e5887b23c753a1498c5c
Ishwarya0108 <159456068+Ishwarya0108@users.noreply.github.com>
2024-03-21 11:08:05 +0530

    Add files via upload
    Upload the files

---
+++ week1/day2/FindOddNumbers.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		
+		int number = 10;
+		
+		for(int i =0; i< number; i++ )
+			
+			if (i%2!=0)
+		{
+			System.out.println("The odd number is:"+i);
+		}}}
---
+++ week1/day2/Homeassignment_palindrome.java
@@ -0,0 +1,13 @@
+package week1.day2;
+
+public class Homeassignment_palindrome {
+
+	public static void main(String[] args) {
+		
+		int input = 121;
+		
+		
+
+	}
+
+}
